9cde8bc263d14873928a2e3f3ffafb00f8a52b35
Pavel Kaburneev <kps23160@gmail.com>
2022-09-23 14:58:02 +0300

    delete bot token

--- create_bot.py
+++ create_bot.py
@@ -2,6 +2,6 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-bot = Bot(token='5702175370:AAE9yk5zdRuQqYeXVD89fctgvqxyqKrGXaw')
+bot = Bot(token='')
 storage = MemoryStorage()
 dispatcher = Dispatcher(bot, storage=storage)
