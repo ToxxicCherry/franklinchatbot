81426fbae5c8932c763a6f57a0c61f0fdfb9ff39
Pavel Kaburneev <kps23160@gmail.com>
2022-09-23 15:56:11 +0300

    hide token to env var

--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,8 @@
 from aiogram import Bot
+import os
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-bot = Bot(token='')
+bot = Bot(token=os.getenv('TOKEN'))
 storage = MemoryStorage()
 dispatcher = Dispatcher(bot, storage=storage)
